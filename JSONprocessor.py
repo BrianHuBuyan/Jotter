43ca3b06c9c152d6af436cd0fed47209cd09e3a0
BrianHuBuyan <brianhbuyan@gmail.com>
2025-01-09 15:41:23 +0800

    Add retrieval function from S3

--- JSONprocessor.py
+++ JSONprocessor.py
@@ -1,13 +1,25 @@
 import csv
 import tscribe
 
-tscribe.write("Input/1.json", format="csv",  save_as="Output/output1.csv")
 
-Transcript = []
-with open('Output/output1.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        Transcript.append([row[3],row[4]])
+def Json2txt(file_name):
+    inputfile = f"Input/{file_name}.json"
+    outputfile = f"Output/{file_name}"
 
-for line in Transcript:
-    print(f"{line[0]}: {line[1]}")
+    tscribe.write(inputfile, format="csv",  save_as= f"{outputfile}.csv")
+
+    Transcript = []
+    with open(f"{outputfile}.csv", newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            Transcript.append([row[3],row[4]])
+
+
+    with open(f"{outputfile}.txt", "w") as file:
+        for line in Transcript:
+            file.write(f"{line[0]}: {line[1]}")  
+            file.write("\n")
+
+# input = "Input/Retrieval_test3"
+# output = "Output/Retrieval_test3"
+Json2txt("Retrieval_test4")
---
+++ debug.py
@@ -0,0 +1,24 @@
+import boto3
+from urllib.parse import urlparse
+import json
+import time
+from Boto3test import get_transcription_result
+
+# Initialize the Transcribe client
+transcribe_client = boto3.client('transcribe')
+s3_client = boto3.client('s3')
+
+response = s3_client.get_object(Bucket="jotter-test-bucket", Key="medical/Retrieval_test3.json")
+
+transcription_json = json.loads(response['Body'].read().decode('utf-8'))
+job_name = "Retrieval_test3"
+with open(f"Input/{job_name}.json", "w") as file:
+    file.write(json.dumps(transcription_json, indent=4))
+
+
+
+# parsed_uri = urlparse("https://s3.us-east-1.amazonaws.com/jotter-test-bucket/medical/Retrieval_test2.json")
+# bucket_name = parsed_uri.netloc
+# object_key = parsed_uri.path.lstrip('/')
+
+# print(object_key)
